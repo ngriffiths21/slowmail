d62fcd8696d8c6385c3e7af65b33372e92873f5b
Nick Griffiths <ngriffiths21@gmail.com>
2024-08-26 09:08:53 -0400

    Generic rendering and minor cleanup
    - `renderPage` renders the template that matches the last part of the request URL
    - Templates are parsed at app init
    - Added function comments

--- app/slowmail.go
+++ app/slowmail.go
@@ -7,35 +7,59 @@ import (
     "flag"
     "log"
     "os"
+    "strings"
 )
 
+// data to pass to the signup page template
 type signupData struct {
     UserExists bool
 }
 
+// parsed templates, will be initialized on app init
+var temps *template.Template
+
+/* internalError
+
+Logs error and sends 500. Use for any error that is an application bug
+or a system error, not a user error.
+*/
 func internalError(writer http.ResponseWriter, err error) {
     log.Println(err.Error())
-    http.Error(writer, err.Error(), http.StatusInternalServerError)
+    http.Error(writer, "The server encountered an error and couldn't fulfill this request. Sorry about that.",
+        http.StatusInternalServerError)
 }
 
-func renderSignup(writer http.ResponseWriter, sdata signupData) {
-    t, err := template.New("new.go.tmpl").ParseFiles("templates/pages/account/new.go.tmpl",
-        "templates/css/styles.go.tmpl")
+/* renderPage
 
-    if (err != nil) {
-        internalError(writer, err)
+Checks the request path and chooses the template that matches the last part of the request path.
+`pdata` is page data, and the proper type depends on the template being rendered.
+*/
+func renderPage(writer http.ResponseWriter, req *http.Request, pdata any) {
+    url := req.URL.Path
+    // if last character is '/', remove it
+    if url[len(url) - 1] == '/' {
+        url = url[:len(url) - 1]
     }
-    err = t.Execute(writer, sdata)
+    // extract last part of path
+    tempName := url[strings.LastIndex(url, "/") + 1:]
+    err := temps.ExecuteTemplate(writer, tempName + ".go.tmpl", pdata)
     if (err != nil) {
         internalError(writer, err)
     }
 }
 
 func getSignup(writer http.ResponseWriter, req *http.Request) {
-    renderSignup(writer, signupData{false})
+    renderPage(writer, req, signupData{false})
 }
 
-func newAccount(writer http.ResponseWriter, req *http.Request) {
+/* postSignup
+
+Parses a signup form and inserts a new user to the database.
+This route handles duplicate username as a user error, and all
+others as internal errors (form validation is the client's
+responsibility).
+*/
+func postSignup(writer http.ResponseWriter, req *http.Request) {
     err := req.ParseForm()
     if (err != nil) {
         internalError(writer, err)
@@ -49,7 +73,7 @@ func newAccount(writer http.ResponseWriter, req *http.Request) {
     if dbErr != nil {
         internalError(writer, dbErr)
     } else if userExists {
-        renderSignup(writer, signupData{true})
+        renderPage(writer, req, signupData{true})
     } else {
         http.Redirect(writer, req, "/", http.StatusSeeOther)
     }
@@ -57,7 +81,7 @@ func newAccount(writer http.ResponseWriter, req *http.Request) {
 
 func startServer() error {
     http.HandleFunc("GET /account/new", getSignup)
-    http.HandleFunc("POST /account/new", newAccount)
+    http.HandleFunc("POST /account/new", postSignup)
 
     err := http.ListenAndServe(":8080", nil)
     return err
@@ -73,7 +97,14 @@ func main() {
         os.Exit(1)
     }
 
-    err := connectDb(dbPath)
+    var err error
+    temps, err = template.ParseFiles("templates/pages/account/new.go.tmpl",
+        "templates/css/styles.go.tmpl")
+    if (err != nil) {
+        log.Panic(err)
+    }
+
+    err = connectDb(dbPath)
     if (err != nil) {
         log.Panic(err)
     }
